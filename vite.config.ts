5e94b27a1ad8149e4092817c341227600be77f8a
alejandro <78042869ale@gmail.com>
2026-06-30 22:41:25 -0400

    vite_corregido

--- vite.config.ts
+++ vite.config.ts
@@ -3,38 +3,42 @@ import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
 import tailwindcss from 'tailwindcss';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    base: process.env.VITE_BASE_PATH || '/build/',
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: env.VITE_BASE_PATH || '/build/',
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+        ],
+        server: {
+            host: true,
+            port: 5173,
+            strictPort: true,
+            cors: true,
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './resources/js'),
             },
-        }),
-    ],
-    server: {
-        host: true, // Permite conexiones desde cualquier dispositivo en la red
-        port: 5173, // Puedes cambiar el puerto si lo deseas
-        strictPort: true,
-        cors: true, // Opcional, permite el CORS
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
         },
-    },
-    css: {
-        postcss: {
-            plugins: [tailwindcss, autoprefixer],
+        css: {
+            postcss: {
+                plugins: [tailwindcss, autoprefixer],
+            },
         },
-    },
+    };
 });
